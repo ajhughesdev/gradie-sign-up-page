53a17b05aec0db05249c67393bdca9b7fb89e235
Andrew J Hughes <andrew@ajhughes.dev>
2022-04-15 18:27:14 -0400

    init commit

---
+++ postcss.config.cjs
@@ -0,0 +1,21 @@
+// PostCSS configuration
+module.exports = (cfg) => {
+
+  // import tokens as variables
+  const variables = require('./tokens.json');
+
+  const 
+    dev = cfg.env === 'development';
+    scss = cfg.file.extname === '.scss';
+  return {
+    map: dev ? { inline: false } : false,
+    parser: scss ? 'postcss-scss' : false,
+    plugins: [
+      require('postcss-import')(),
+      require('postcss-advanced-variables')({ variables }),
+      require('postcss-nested')(),
+      require('autoprefixer')(),
+      dev ? null : require('cssnano')(),
+    ],
+  };
+};
